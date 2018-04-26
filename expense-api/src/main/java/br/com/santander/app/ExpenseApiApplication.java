7af5d4d71590dc0d914d2535f4123e660a7c96d9
elton.correa <elton.correa>
2018-04-26 10:22:09 -0300

    Atualizando estrutura do projeto.

---
+++ expense-api/src/main/java/br/com/santander/app/ExpenseApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.santander.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseApiApplication.class, args);
+	}
+}

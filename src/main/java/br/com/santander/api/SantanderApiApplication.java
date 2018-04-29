04acd40bfd5509218da9d7853ee6c666ede59ef2
Diego Hortolani Fernandes <diegofernandes88@hotmail.com>
2018-04-29 11:35:56 -0300

    Criação do main Application e configuração do arquivo "application.properties".

--- src/main/java/br/com/santander/api/SantanderApiApplication.java
+++ src/main/java/br/com/santander/api/SantanderApiApplication.java
@@ -1,8 +1,12 @@
 package br.com.santander.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
 public class SantanderApiApplication {
 

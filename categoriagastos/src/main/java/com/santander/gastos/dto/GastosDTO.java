b9890a6e6f904ce17aef2d241442ad142cf0d82f
Marc <marc.alcantara@mfconsulting.com.br>
2018-05-02 05:56:26 -0300

    Representa os dados expostos fora do boundary da api, serve tanto para registrar novos gastos como para mostrar na tela do mobile seus detalhes.

--- categoriagastos/src/main/java/com/santander/gastos/dto/GastosDTO.java
+++ categoriagastos/src/main/java/com/santander/gastos/dto/GastosDTO.java
@@ -1,5 +1,71 @@
 package com.santander.gastos.dto;
 
-public class GastosDTO {
+import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
+import com.santander.gastos.domain.Movimento;
+
+public class GastosDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	int id;
+	String descricao;
+	double valor;
+	String data;
+	Long codigoCliente;
+	CategoriaDTO categoria;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public GastosDTO() {
+		super();
+	}
+	public CategoriaDTO getCategoria() {
+		return categoria;
+	}
+	public void setCategoria(CategoriaDTO categoria) {
+		this.categoria = categoria;
+	}
+	public Long getCodigoCliente() {
+		return codigoCliente;
+	}
+	public void setCodigoCliente(Long codigoCliente) {
+		this.codigoCliente = codigoCliente;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	
+	public GastosDTO(Movimento movimento) {
+		
+		DateFormat   df = new SimpleDateFormat("yyyy-MM-dd");
+		
+		this.codigoCliente = movimento.getCartao().getConta().getCliente().getCodigoUsuario();
+		this.descricao = movimento.getDescricao();
+		this.valor = movimento.getValor();
+		this.data = df.format( movimento.getData());
+		this.categoria  = new CategoriaDTO( movimento.getCategoria());
+		
+	}
+	
 }

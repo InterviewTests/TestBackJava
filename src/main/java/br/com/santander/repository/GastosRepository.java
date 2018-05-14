b1e2531b9e6b6ae99c7fa507e3effdbe419d1a36
Flávio Dantas <fdantasb@gmail.com>
2018-05-14 20:46:04 -0300

    Criado metodo para buscar todos os gastos usando o numero de usuario

--- src/main/java/br/com/santander/repository/GastosRepository.java
+++ src/main/java/br/com/santander/repository/GastosRepository.java
@@ -3,6 +3,9 @@ package br.com.santander.repository;
 import br.com.santander.domain.Gasto;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface GastosRepository extends PagingAndSortingRepository<Gasto, String> {
 
+    List<Gasto> findAllByCodigoUsuario(long codigoUsuario);
 }

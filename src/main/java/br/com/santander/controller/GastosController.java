6fb5842ead09e658b7ea4a0846ed3334ce350ca5
Flávio Dantas <fdantasb@gmail.com>
2018-05-14 18:54:09 -0300

    adicionada injecado de servico de gastos e sua primeira chamada para salvar o gasto da tela

--- src/main/java/br/com/santander/controller/GastosController.java
+++ src/main/java/br/com/santander/controller/GastosController.java
@@ -2,6 +2,8 @@ package br.com.santander.controller;
 
 import br.com.santander.ServletInitializer;
 import br.com.santander.domain.Gasto;
+import br.com.santander.service.GastoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,12 @@ import java.net.URI;
 @RestController
 @RequestMapping("/gastos")
 public class GastosController {
+
+    @Autowired
+    private GastoService gastoService;
+
     public ResponseEntity<Void> incluir(Gasto gasto) {
-
-
+        gasto = gastoService.incluir(gasto);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(gasto.getId()).toUri();
         return ResponseEntity.created(uri).build();

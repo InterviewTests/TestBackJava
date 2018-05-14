7ea3c3cc92dc1611c65913e96feb26f7f8f4cb03
Flávio Dantas <fdantasb@gmail.com>
2018-05-14 18:52:02 -0300

    Adicionadas demais propriedades na classe de gasto

--- src/main/java/br/com/santander/domain/Gasto.java
+++ src/main/java/br/com/santander/domain/Gasto.java
@@ -1,4 +1,55 @@
 package br.com.santander.domain;
 
+import org.springframework.data.cassandra.core.mapping.Table;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table
 public class Gasto {
+    private UUID id;
+    private String descricao;
+    private Double valor;
+    private long codigousuario;
+    private Date data;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public long getCodigousuario() {
+        return codigousuario;
+    }
+
+    public void setCodigousuario(long codigousuario) {
+        this.codigousuario = codigousuario;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
 }

6e2c36a483ca99342d996914a93351617e5c53df
Flávio Dantas <fdantasb@gmail.com>
2018-05-14 18:52:18 -0300

    Adicionado metodo inicial de inclusao de gastos

--- src/main/java/br/com/santander/controller/GastosController.java
+++ src/main/java/br/com/santander/controller/GastosController.java
@@ -1,4 +1,22 @@
 package br.com.santander.controller;
 
+import br.com.santander.ServletInitializer;
+import br.com.santander.domain.Gasto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/gastos")
 public class GastosController {
+    public ResponseEntity<Void> incluir(Gasto gasto) {
+
+
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(gasto.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
 }

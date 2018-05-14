54c8496f7343d1eaa0c5432f31db677344e65167
Flávio Dantas <fdantasb@gmail.com>
2018-05-14 20:46:45 -0300

    Ajustado como voltar os gastos somente 5 segundos mais velhos

--- src/main/java/br/com/santander/service/GastoService.java
+++ src/main/java/br/com/santander/service/GastoService.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class GastoService {
@@ -18,5 +21,13 @@ public class GastoService {
             gasto.setData(Calendar.getInstance().getTime());
         }
         return repository.save(gasto);
+    }
+
+    public List<Gasto> listarGastos(long user) {
+        List<Gasto> result = repository.findAllByCodigoUsuario(user);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.SECOND, -5);
+        return result.stream().filter(g -> g.getData().before(calendar.getTime())).collect(Collectors.toList());
     }
 }

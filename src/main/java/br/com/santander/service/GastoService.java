3b901387c88b784d00b35d9f59e331581a400205
Flávio Dantas <fdantasb@gmail.com>
2018-05-14 18:53:50 -0300

    criado metodo de inclusao de gasto

--- src/main/java/br/com/santander/service/GastoService.java
+++ src/main/java/br/com/santander/service/GastoService.java
@@ -1,4 +1,14 @@
 package br.com.santander.service;
 
+import br.com.santander.domain.Gasto;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GastoService {
+    public Gasto incluir(Gasto gasto) {
+
+        
+
+        return null;
+    }
 }

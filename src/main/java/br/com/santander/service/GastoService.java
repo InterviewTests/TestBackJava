a7e61a885124341322076f37e45ff41db6fa70d0
Flávio Dantas <fdantasb@gmail.com>
2018-05-14 19:01:25 -0300

    ajustes no servico para salvar o gasto

--- src/main/java/br/com/santander/service/GastoService.java
+++ src/main/java/br/com/santander/service/GastoService.java
@@ -1,14 +1,15 @@
 package br.com.santander.service;
 
 import br.com.santander.domain.Gasto;
+import br.com.santander.repository.GastosRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GastoService {
+
+    private GastosRepository repository;
+
     public Gasto incluir(Gasto gasto) {
-
-        
-
-        return null;
+        return repository.save(gasto);
     }
 }

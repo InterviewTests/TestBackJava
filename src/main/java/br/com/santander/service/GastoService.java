c136e526ca67f1e6d1c6b3e20dc8c923f4628a91
Flávio Dantas <fdantasb@gmail.com>
2018-05-14 20:10:41 -0300

    Ajustes para data vazia

--- src/main/java/br/com/santander/service/GastoService.java
+++ src/main/java/br/com/santander/service/GastoService.java
@@ -4,12 +4,19 @@ import br.com.santander.domain.Gasto;
 import br.com.santander.repository.GastosRepository;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
+import java.util.Calendar;
+
 @Service
 public class GastoService {
 
+    @Resource
     private GastosRepository repository;
 
     public Gasto incluir(Gasto gasto) {
+        if (gasto.getData() == null) {
+            gasto.setData(Calendar.getInstance().getTime());
+        }
         return repository.save(gasto);
     }
 }

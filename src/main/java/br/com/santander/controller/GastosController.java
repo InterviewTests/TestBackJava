7b248a25984da840f69d332ffd7c8857bb8d9576
Flávio Dantas <fdantasb@gmail.com>
2018-05-14 20:11:03 -0300

    criado o método para listagem de gastos por usuario

--- src/main/java/br/com/santander/controller/GastosController.java
+++ src/main/java/br/com/santander/controller/GastosController.java
@@ -5,12 +5,15 @@ import br.com.santander.domain.Gasto;
 import br.com.santander.service.GastoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/gastos")
@@ -20,9 +23,11 @@ public class GastosController {
     private GastoService gastoService;
 
     @RequestMapping(value = "/incluir", method = RequestMethod.POST)
-    public ResponseEntity<Void> incluir(Gasto gasto) {
+    public ResponseEntity<Void> incluir(@Valid @RequestBody Gasto gasto) {
         gasto = gastoService.incluir(gasto);
-        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(gasto.getId()).toUri();
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{usuario}").buildAndExpand(gasto.getCodigousuario()).toUri();
         return ResponseEntity.created(uri).build();
     }
+
+    public ResponseEntity<List<Gasto>> listarGastos(long )
 }

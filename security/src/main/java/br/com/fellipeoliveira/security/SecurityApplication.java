084ec0d094135b00d64909484f4d9638665b9875
Fellipe Soares Oliveira <fellipe.soares@barad-dur.local>
2018-12-28 13:05:16 -0200

    Criado o microserviço de segurança, para fazer autenticação

---
+++ security/src/main/java/br/com/fellipeoliveira/security/SecurityApplication.java
@@ -0,0 +1,14 @@
+package br.com.fellipeoliveira.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
+

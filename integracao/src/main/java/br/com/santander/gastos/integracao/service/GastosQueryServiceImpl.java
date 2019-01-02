cda4f583da690aa9d5064420e1ce09006f33b707
Henrique Lucas França <hlucasfranca@gmail.com>
2019-01-02 13:57:58 -0200

    Adiciona QueryDSL para consultas fluentes.

--- integracao/src/main/java/br/com/santander/gastos/integracao/service/GastosQueryServiceImpl.java
+++ integracao/src/main/java/br/com/santander/gastos/integracao/service/GastosQueryServiceImpl.java
@@ -2,8 +2,10 @@ package br.com.santander.gastos.integracao.service;
 
 import br.com.santander.gastos.integracao.dto.GastosDTO;
 import br.com.santander.gastos.integracao.entity.GastoEntity;
+import br.com.santander.gastos.integracao.entity.QGastoEntity;
 import br.com.santander.gastos.integracao.mappers.GastosMapper;
 import br.com.santander.gastos.integracao.repository.GastoRepository;
+import com.querydsl.core.BooleanBuilder;
 import org.mapstruct.factory.Mappers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -11,6 +13,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -28,7 +31,14 @@ public class GastosQueryServiceImpl implements GastosQueryService {
 
     @Override
     public Page<GastosDTO> consultarGastos(Long codigoUsuario, Pageable pageable) {
-        final Page<GastoEntity> page = gastoRepository.findByCodigoUsuario(codigoUsuario, pageable);
+
+        BooleanBuilder builder = new BooleanBuilder();
+        QGastoEntity qGasto = QGastoEntity.gastoEntity;
+
+        builder.and(qGasto.data.between(LocalDateTime.now().minusSeconds(5), LocalDateTime.now()));
+        builder.and(qGasto.codigoUsuario.eq(codigoUsuario));
+
+        final Page<GastoEntity> page = gastoRepository.findAll(builder, pageable);
 
         final List<GastosDTO> dtoList = page.get()
                 .map(gastosMapper::entityToDTO)

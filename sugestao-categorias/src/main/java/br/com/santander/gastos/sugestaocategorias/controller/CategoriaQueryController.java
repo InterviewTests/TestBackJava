9c95a55c55273f6c92dcfa6dbf2899e0cac7f553
Henrique Lucas França <hlucasfranca@gmail.com>
2019-01-03 21:40:43 -0200

    Dockerização da API de categorias.

--- sugestao-categorias/src/main/java/br/com/santander/gastos/sugestaocategorias/controller/CategoriaQueryController.java
+++ sugestao-categorias/src/main/java/br/com/santander/gastos/sugestaocategorias/controller/CategoriaQueryController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
+@RequestMapping(path="/api/v1")
 public class CategoriaQueryController {
 
     private final CategoriaQueryService categoriaQueryService;

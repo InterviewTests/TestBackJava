1f42425e2aee97565673087faa33ff218e3f8ad0
Henrique Lucas França <hlucasfranca@gmail.com>
2019-01-04 07:44:20 -0200

    Faz a separação de responsabilidades.

---
+++ gastos-query-api/src/main/java/br/com/santander/gastos/query/GastosQueryApplication.java
@@ -0,0 +1,16 @@
+package br.com.santander.gastos.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GastosQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GastosQueryApplication.class, args);
+    }
+
+}
+

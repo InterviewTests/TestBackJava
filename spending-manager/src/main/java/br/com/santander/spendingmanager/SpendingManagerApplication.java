27b357e6d5d6fbdd5a6854584cf1b1da4f940b58
fegsi12 <devtest@fgomes.eti.br>
2019-01-23 15:40:07 -0200

    initial commit

---
+++ spending-manager/src/main/java/br/com/santander/spendingmanager/SpendingManagerApplication.java
@@ -0,0 +1,14 @@
+package br.com.santander.spendingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpendingManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpendingManagerApplication.class, args);
+    }
+
+}
+

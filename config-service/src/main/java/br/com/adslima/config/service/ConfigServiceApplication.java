8a63e167c15e2bb92424f4da5aa65dae51895124
Silva <andrews.silva@accenture.com>
2019-02-05 11:36:22 -0300

    Documentação JavaDoc

--- config-service/src/main/java/br/com/adslima/config/service/ConfigServiceApplication.java
+++ config-service/src/main/java/br/com/adslima/config/service/ConfigServiceApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * 
+ * @author andrews.silva
+ *
+ */
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServiceApplication {

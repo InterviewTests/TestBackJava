ee17ec96859cad567992f17bc5a1c32ea441a1cf
Silva <andrews.silva@accenture.com>
2019-03-06 01:18:48 -0300

    Ajustes nos serviços de registro e balanceamento de carga

---
+++ service-registry/src/main/java/br/com/adslima/ServiceRegistryApplication.java
@@ -0,0 +1,19 @@
+package br.com.adslima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 
+ * @author andrews.silva
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegistryApplication.class, args);
+	}
+}

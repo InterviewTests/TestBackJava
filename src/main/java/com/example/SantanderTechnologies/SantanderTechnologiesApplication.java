a9ed054f1ba7f6f981aa46bb660f8ed9f002b61c
Thadeu Antonio Ferreira de Melo <thadeu_afmelo@hotmail.com>
2019-05-13 22:57:09 -0300

    first commit

---
+++ src/main/java/com/example/SantanderTechnologies/SantanderTechnologiesApplication.java
@@ -0,0 +1,13 @@
+package com.example.SantanderTechnologies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderTechnologiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderTechnologiesApplication.class, args);
+	}
+
+}

dfd3257f6db3558e99ce729fd2ecacb33d3dee7c
Thadeu Antonio Ferreira de Melo <thadeu_afmelo@hotmail.com>
2019-05-14 16:30:04 -0300

    Add JWT

---
+++ src/main/java/com/example/SantanderTechnologies/JWT/InvalidJwtAuthenticationException.java
@@ -0,0 +1,9 @@
+package com.example.SantanderTechnologies.JWT;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class InvalidJwtAuthenticationException extends AuthenticationException {
+    public InvalidJwtAuthenticationException(String e) {
+        super(e);
+    }
+}

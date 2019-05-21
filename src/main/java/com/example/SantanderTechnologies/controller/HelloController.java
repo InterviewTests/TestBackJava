677d41c75617bc709e30179cb89c75a4d7b4b614
Thadeu Antonio Ferreira de Melo <thadeu_afmelo@hotmail.com>
2019-05-20 23:43:08 -0300

    Add Hello world controller

---
+++ src/main/java/com/example/SantanderTechnologies/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.SantanderTechnologies.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+}

a556ef238368568fca40e9b86afd6eff2194f12d
eliseu_germano <eliseu.germano1@gmail.com>
2019-06-07 08:02:35 -0300

    Adicionando projeto Spring-boot

---
+++ gastos-api/src/main/java/com/santander/gastosapi/GastosApiApplication.java
@@ -0,0 +1,13 @@
+package com.santander.gastosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GastosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GastosApiApplication.class, args);
+	}
+
+}

e355d41ec7359c37245acd9bf43ba8935f45c3fd
warley.rocha <warley.rocha@zup.com.br>
2019-07-01 15:22:56 +0100

    Configuração do Sistema

---
+++ src/main/java/br/com/zup/way/WayApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.way;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WayApplication.class, args);
+    }
+
+}

c69c0fbca6faa165bac07127668ff70ad2c88ade
Jônatas Santos <jonatas.santos@zup.com.br>
2019-07-06 17:51:15 -0300

    Refatorando para simplificar a aplicação: - Remoção de classes que não serão mais utilizadas

--- testeSantanderWay/src/main/java/br/com/testesantanderway/config/SwaggerConfig.java
+++ testeSantanderWay/src/main/java/br/com/testesantanderway/config/SwaggerConfig.java
@@ -1,15 +1,18 @@
-package br.com.testesantanderway.util.swagger;
+package br.com.testesantanderway.config;
 
-import br.com.testesantanderway.modelo.Cliente;
+import br.com.testesantanderway.modelo.Sistema;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -22,7 +25,13 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("br.com.testesantanderway.controller"))
                 .build()
                 .apiInfo(apiInfo())
-                .ignoredParameterTypes(Cliente.class);
+                .ignoredParameterTypes(Sistema.class).globalOperationParameters(
+                        Arrays.asList(new ParameterBuilder()
+                                .name("Authorization")
+                                .description("Header for Token JWT")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false).build()));
     }
 
     private ApiInfo apiInfo() {

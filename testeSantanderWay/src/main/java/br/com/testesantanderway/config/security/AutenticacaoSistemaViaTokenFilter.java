b6198b6cfa03dd0f71f7a70f203009663e710102
Jônatas Santos <jonatas.santos@zup.com.br>
2019-07-07 00:26:34 -0300

    Refatorando autoricação: -Separada autenticação de Sistema e Usuario para que cada autenticação utilize seu própria entidade. - Ajustes no formato de data para facilitar a serializaçao de datas com o Solr

--- testeSantanderWay/src/main/java/br/com/testesantanderway/config/security/AutenticacaoSistemaViaTokenFilter.java
+++ testeSantanderWay/src/main/java/br/com/testesantanderway/config/security/AutenticacaoSistemaViaTokenFilter.java
@@ -1,7 +1,10 @@
 package br.com.testesantanderway.config.security;
 
 import br.com.testesantanderway.modelo.Sistema;
+import br.com.testesantanderway.modelo.Usuario;
 import br.com.testesantanderway.repository.SistemaRepository;
+import br.com.testesantanderway.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -10,18 +13,20 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
 
     private static final String BEARER = "Bearer ";
 
+    @Autowired
     private ServicoDeToken tokenService;
-    private SistemaRepository repository;
 
-    public AutenticacaoViaTokenFilter(ServicoDeToken tokenService, SistemaRepository repository) {
-        this.tokenService = tokenService;
-        this.repository = repository;
-    }
+    @Autowired
+    private SistemaRepository sistemaRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
@@ -29,15 +34,23 @@ public class AutenticacaoViaTokenFilter extends OncePerRequestFilter {
         String token = AutenticacaoViaTokenFilter.recuperarToken(request);
         boolean valido = tokenService.isTokenValido(token);
         if (valido){
-            autenticarCliente(token);
+            autenticarUsuario(token);
         }
 
         filterChain.doFilter(request, response);
     }
 
-    private void autenticarCliente(String token) {
-        String idCliente = tokenService.getCodigo(token);
-        Sistema sistema = repository.findById(idCliente).get();
+    private void autenticarUsuario(String token) {
+        String codigoUsuario = tokenService.getCodigo(token);
+        Usuario usuario = usuarioRepository.findById(codigoUsuario).get();
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(usuario,
+                null, Arrays.asList(() -> "USUARIO"));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private void autenticarSistema(String token) {
+        String codigoSistema = tokenService.getCodigo(token);
+        Sistema sistema = sistemaRepository.findById(codigoSistema).get();
         UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(sistema,
                 null, sistema.getAuthorities());
         SecurityContextHolder.getContext().setAuthentication(authentication);

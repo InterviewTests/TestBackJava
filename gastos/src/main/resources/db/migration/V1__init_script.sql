b7b5054a3f79b4569d12917c94ff949f0fc499ae
Daniel Kiluange Assiz <daniel.kiluange@gmail.com>
2019-07-08 01:28:43 -0300

    modificação flyway

--- gastos/src/main/resources/db/migration/V1__init_script.sql
+++ gastos/src/main/resources/db/migration/V1__init_script.sql
@@ -24,6 +24,14 @@ CREATE TABLE client (
   PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+CREATE TABLE classification (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  name varchar(255) DEFAULT NULL,
+  dat_creation datetime NOT NULL,
+  dat_update datetime NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 CREATE TABLE card_spent (
   id bigint(20) NOT NULL AUTO_INCREMENT,
   dat_creation datetime NOT NULL,
@@ -48,4 +56,17 @@ CREATE TABLE client_card (
   KEY FK_card_2 (card),
   CONSTRAINT FK_card_2 FOREIGN KEY (card) REFERENCES card (id),
   CONSTRAINT FK_client FOREIGN KEY (client) REFERENCES client (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE client_classification (
+  id bigint(20) NOT NULL AUTO_INCREMENT,
+  dat_creation datetime NOT NULL,
+  dat_update datetime NOT NULL,
+  client bigint(20) DEFAULT NULL,
+  classification bigint(20) DEFAULT NULL,
+  PRIMARY KEY (id),
+  KEY FK_client (client),
+  KEY FK_card_2 (card),
+  CONSTRAINT FK_classification FOREIGN KEY (classification) REFERENCES classification (id),
+  CONSTRAINT FK_client_2 FOREIGN KEY (client) REFERENCES client (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;

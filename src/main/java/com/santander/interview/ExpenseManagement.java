6648577047aa2ce27ea9a6a7a933898765810e5f
Thiago Sato <p000tsato@prservicos.com.br>
2019-09-07 12:38:05 -0300

    Adicionando configurações para execução com threads

--- src/main/java/com/santander/interview/ExpenseManagement.java
+++ src/main/java/com/santander/interview/ExpenseManagement.java
@@ -3,7 +3,9 @@ package com.santander.interview;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.santander.interview"})
 public class ExpenseManagement {
--- src/main/java/com/santander/interview/config/ThreadConfig.java
+++ src/main/java/com/santander/interview/config/ThreadConfig.java
@@ -1,4 +1,36 @@
 package com.santander.interview.config;
 
-public class ThredConfig {
+import com.santander.interview.utils.ExpenseManagementUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+public class ThreadConfig {
+    public static final String THREAD_NAME_PREFIX = "interview";
+
+    @Value("#{environment['ASYNC_CORE_POOL_SIZE']}")
+    private String ASYNC_CORE_POOL_SIZE;
+
+    @Value("#{environment['ASYNC_MAX_POOL_SIZE']}")
+    private String ASYNC_MAX_POOL_SIZE;
+
+    @Bean
+    public Executor asyncExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(
+                ExpenseManagementUtils.expenseManagementUtils(10, ASYNC_CORE_POOL_SIZE)
+        );
+        threadPoolTaskExecutor.setMaxPoolSize(
+                ExpenseManagementUtils.expenseManagementUtils(100, ASYNC_MAX_POOL_SIZE)
+        );
+        threadPoolTaskExecutor.setQueueCapacity(Integer.MAX_VALUE);
+        threadPoolTaskExecutor.setThreadNamePrefix(THREAD_NAME_PREFIX);
+        threadPoolTaskExecutor.initialize();
+
+        return threadPoolTaskExecutor;
+    }
 }

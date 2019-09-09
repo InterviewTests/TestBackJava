dfc11a965b28a76dff2f79921575bc25f5fa757a
Thiago Sato <p000tsato@prservicos.com.br>
2019-09-08 22:25:44 -0300

    Adicionando Swagger para documentação das APIs

--- src/main/java/com/santander/interview/controller/CategoryController.java
+++ src/main/java/com/santander/interview/controller/CategoryController.java
@@ -5,6 +5,9 @@ import static com.santander.interview.enums.ResponseMessageEnum.*;
 import com.santander.interview.domain.Category;
 import com.santander.interview.domain.Response;
 import com.santander.interview.service.CategoryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,13 +16,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/expense-management")
+@Api(value = "Categoria")
 public class CategoryController {
 
     @Autowired
     CategoryService categoryService;
 
+    @ApiOperation("Adicionar uma nova categoria")
     @PostMapping("/categories")
-    public ResponseEntity<Response> addCategory(@RequestBody Category category) {
+    public ResponseEntity<Response> addCategory(
+            @ApiParam(value = "Nova categoria", required = true) @RequestBody Category category
+    ) {
         categoryService.saveCategory(category);
 
         return new ResponseEntity<>(
@@ -28,8 +36,11 @@ public class CategoryController {
         );
     }
 
+    @ApiOperation("Sugestão de categoria")
     @GetMapping("/category/detail/{detailPrefix}")
-    public ResponseEntity<Response> suggestionCategory(@PathVariable String detailPrefix) {
+    public ResponseEntity<Response> suggestionCategory(
+            @ApiParam(value = "Prefixo da categoria", required = true) @PathVariable String detailPrefix
+    ) {
         List<Category> categories = this.categoryService.searchCategoryByDetailPrefix(detailPrefix);
 
         return new ResponseEntity<>(
--- src/main/java/com/santander/interview/controller/ExpenseController.java
+++ src/main/java/com/santander/interview/controller/ExpenseController.java
@@ -5,6 +5,7 @@ import static com.santander.interview.enums.ResponseMessageEnum.*;
 import com.santander.interview.domain.Expense;
 import com.santander.interview.domain.Response;
 import com.santander.interview.service.ExpenseService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,13 +15,18 @@ import java.text.ParseException;
 import java.util.List;
 
 @RestController
+@RequestMapping("/expense-management")
+@Api(value = "Gasto")
 public class ExpenseController {
 
     @Autowired
     ExpenseService expenseService;
 
+    @ApiOperation(value = "Adicionar gasto")
     @PostMapping("/expenses")
-    public ResponseEntity<Response> addExpense(@RequestBody Expense expense) {
+    public ResponseEntity<Response> addExpense(
+            @ApiParam(value = "Novo gasto", required = true) @RequestBody Expense expense
+    ) {
         this.expenseService.addNewExpense(expense);
         return new ResponseEntity<>(
                 new Response(HttpStatus.OK.value(), ADD_EXPENSE_SUCCESS.getMessage(), null),
@@ -28,9 +34,12 @@ public class ExpenseController {
         );
     }
 
+    @ApiOperation("Buscar gastos por usuário")
     @GetMapping("/expense/userCode/{userCode}")
-    public ResponseEntity<Response> getExpenseByUserCode(@PathVariable long userCode) {
-        List<Expense> expensesByUserCode = this.expenseService.findExpensesByCodigoUsuario(userCode);
+    public ResponseEntity<Response> getExpenseByUserCode(
+            @ApiParam(value = "Código do cliente", required = true) @PathVariable long userCode
+    ) {
+        List<Expense> expensesByUserCode = this.expenseService.findExpensesByUserCode(userCode);
         return new ResponseEntity<>(
                 new Response(HttpStatus.OK.value(), SEARCH_EXPENSE_BY_USER_CODE_SUCCESS.getMessage(),
                         expensesByUserCode),
@@ -38,10 +47,14 @@ public class ExpenseController {
         );
     }
 
+    @ApiOperation("Buscar gastos por usuário e data")
     @GetMapping("/expense/userCode/{userCode}/date/{date}")
-    public ResponseEntity<Response> getExpenseByUserCodeAndDate(@PathVariable long userCode, @PathVariable String date) {
+    public ResponseEntity<Response> getExpenseByUserCodeAndDate(
+            @ApiParam(value = "Código do cliente", required = true) @PathVariable long userCode,
+            @ApiParam(value = "Data a ser pesquisada", required = true) @PathVariable String date
+    ) {
         try {
-            List<Expense> expensesByUserCodeAndDate = this.expenseService.findExpensesByCodigoUsuarioAndData(userCode, date);
+            List<Expense> expensesByUserCodeAndDate = this.expenseService.findExpensesByUserCodeAndDate(userCode, date);
             return new ResponseEntity<>(
                     new Response(HttpStatus.OK.value(), SEARCH_EXPENSE_BY_USER_CODE_AND_DATE_SUCCESS.getMessage(),
                             expensesByUserCodeAndDate),
@@ -55,8 +68,12 @@ public class ExpenseController {
         }
     }
 
+    @ApiOperation("Atualizar gasto")
     @PutMapping("/expense/{id}")
-    public ResponseEntity<Response> updateExpense(@PathVariable String id, @RequestBody Expense expense) {
+    public ResponseEntity<Response> updateExpense(
+            @ApiParam(value = "ID do gasto a ser atualizado", required = true) @PathVariable String id,
+            @ApiParam(value = "Gasto atualizado", required = true) @RequestBody Expense expense
+    ) {
         this.expenseService.updateExpense(id, expense);
 
         return new ResponseEntity<>(

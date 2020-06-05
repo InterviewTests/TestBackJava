21f33a04745fb7c828f9f13287a1aa6c7032e5b4
Guilherme Raulino de Faria <guilherme.faria@digisystem.com.br>
2020-06-05 19:33:42 -0300

    Criação do Projeto e .gitignore
    Nesse primeiro commit, foi feita a criação da base do projeto através do Spring Initializr com as dependências necessárias. Entre elas, a dependência do Banco de Dados que estaremos usando o H2 e o JPA para a persistência dos dados, Lombok para um código mais limpo e as ferramentas necessárias para o desenvolvimento das API's

---
+++ gft/src/main/java/com/teste/gft/GftApplication.java
@@ -0,0 +1,13 @@
+package com.teste.gft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GftApplication.class, args);
+	}
+
+}
---
+++ gft/src/test/java/com/teste/gft/GftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teste.gft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
